7b5b174c18aa3299316f72bfb5438696683d865b
Hamza Khan <hamza.khan.9440@gmail.com>
2018-07-19 01:28:50 -0700

    Optimized Flipbit method added

--- Bit-Manipulation/flipBit.java
+++ Bit-Manipulation/flipBit.java
@@ -12,10 +12,10 @@ public class flipBit {
 
 	public static void main(String[] args) {
 		System.out.println(longestSequence(1775));
-		
-
+		System.out.println(flipbit(1775)); 
 	}
 
+	//==== This method takes O(bits) time and O(bits) space
 	public static int longestSequence(int n){
 		if(n==-1)
 			return Integer.BYTES*8;
@@ -73,6 +73,40 @@ public class flipBit {
 		}
 		return maxSeq;
 	}
+	// =================================================================
+	
+	
+	
+	
+	/*
+	 *  Optimized Flipbit that does the job in O(bits) time and O(1) space
+	 */
+	public static int flipbit(int a){
+//		If all 1s, this is already the longest sequence
+		if(~a ==0)
+			return Integer.BYTES * 8;
+		
+		int currentLength =0;
+		int previousLength = 0;
+		int maxLength = 1;
+		
+		while(a!=0){
+			if( (a&1) == 1 ){//current bit is a 1
+				currentLength++;
+			}
+			else if( (a&1) == 0){// current bit is a 0
+				currentLength++;
+//				Update to 0 (if next bit is 0) or currentLength (if next bit is 1)
+				previousLength = (a&2) == 0 ? 0 : currentLength;
+				currentLength = 0;
+			}
+			maxLength =  Math.max(previousLength + currentLength + 1, maxLength );
+			a>>>=1;
+		}
+		return maxLength;
+			
+	}
+	
 	
 	
 }
